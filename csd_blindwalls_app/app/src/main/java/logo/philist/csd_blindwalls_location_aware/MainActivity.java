f212bbf3630c94d80f0cf3e0aee32493b4f854b6
Logophilist <Ralf@van-aert.nl>
2020-12-18 10:35:28 +0100

    Redone mainview and recyclerview set up

--- csd_blindwalls_app/app/src/main/java/logo/philist/csd_blindwalls_location_aware/MainActivity.java
+++ csd_blindwalls_app/app/src/main/java/logo/philist/csd_blindwalls_location_aware/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExtendedFloatingActionButton fabMurals = findViewById(R.id.fabButton_toMurals);
+        ExtendedFloatingActionButton fabRoutes = findViewById(R.id.fabButton_toRoutes);
     }
 }
