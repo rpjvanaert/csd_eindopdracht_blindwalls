f2f5ff9ba4861ec0a55304846b0b5d088cef168d
Logophilist <Ralf@van-aert.nl>
2020-12-21 15:27:12 +0100

    mainactivity to route and mural side done

--- csd_blindwalls_app/app/src/main/java/logo/philist/csd_blindwalls_location_aware/Views/MainActivity.java
+++ csd_blindwalls_app/app/src/main/java/logo/philist/csd_blindwalls_location_aware/Views/MainActivity.java
@@ -1,18 +1,25 @@
 package logo.philist.csd_blindwalls_location_aware.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStore;
+import androidx.lifecycle.ViewModelStoreOwner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
-import org.osmdroid.util.GeoPoint;
-
-import java.util.Locale;
-
 import logo.philist.csd_blindwalls_location_aware.R;
+import logo.philist.csd_blindwalls_location_aware.ViewModels.MuralsViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_VIEWMODEL_STORE_OWNER = MainActivity.class.getName() + "_VIEWMODEL_STORE_OWNER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         ExtendedFloatingActionButton fabMurals = findViewById(R.id.fabButton_toMurals);
         ExtendedFloatingActionButton fabRoutes = findViewById(R.id.fabButton_toRoutes);
+
+        fabMurals.setOnClickListener((view -> {
+            Log.i(MainActivity.class.getName(), "clicked fab Murals");
+            Intent intent = new Intent(this, MuralsListActivity.class);
+            MuralsViewModel muralsViewModel = new ViewModelProvider(this).get(MuralsViewModel.class);
+            startActivity(intent);
+        }));
+
+        fabRoutes.setOnClickListener(view -> {
+            Log.i(MainActivity.class.getName(), "clicked fab Routes");
+            Intent intent = new Intent(this, RouteListActivity.class);
+            startActivity(intent);
+        });
     }
 }
